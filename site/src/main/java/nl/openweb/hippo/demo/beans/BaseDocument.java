2e885adc7647db24ad6eadcb45b1ec2a965c480b
Ebrahim aharpour <ebrahim@openweb.nl>
2016-04-06 14:15:49 +0200

    Initial commit

---
+++ site/src/main/java/nl/openweb/hippo/demo/beans/BaseDocument.java
@@ -0,0 +1,9 @@
+package nl.openweb.hippo.demo.beans;
+
+import org.hippoecm.hst.content.beans.Node;
+import org.hippoecm.hst.content.beans.standard.HippoDocument;
+
+@Node(jcrType="outofboxhippo:basedocument")
+public class BaseDocument extends HippoDocument {
+
+}
